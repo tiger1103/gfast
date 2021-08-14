7f7f97be94509138e4d560544e1ff807cdbd1076
yxh <yxh1103@qq.com>
2021-08-14 16:22:24 +0800

    修复在线用户分页

--- app/common/model/req.go
+++ app/common/model/req.go
@@ -7,7 +7,7 @@ const PageSize = 10
 type PageReq struct {
 	BeginTime string `p:"beginTime"` //开始时间
 	EndTime   string `p:"endTime"`   //结束时间
-	PageNum   int    `p:"PageNum"`   //当前页码
+	PageNum   int    `p:"pageNum"`   //当前页码
 	PageSize  int    `p:"pageSize"`  //每页数
 	Ctx       context.Context
 	OrderBy   string //排序方式
