2f2427269aa9d6fce33ed8e2856068a8e3f7d6e2
yxh <yxh1103@qq.com>
2021-09-01 21:00:14 +0800

    swagger

--- app/common/model/req.go
+++ app/common/model/req.go
@@ -5,12 +5,12 @@ import "context"
 const PageSize = 10
 
 type PageReq struct {
-	BeginTime string `p:"beginTime"` //开始时间
-	EndTime   string `p:"endTime"`   //结束时间
-	PageNum   int    `p:"pageNum"`   //当前页码
-	PageSize  int    `p:"pageSize"`  //每页数
-	Ctx       context.Context
-	OrderBy   string //排序方式
+	BeginTime string          `p:"beginTime"` //开始时间
+	EndTime   string          `p:"endTime"`   //结束时间
+	PageNum   int             `p:"pageNum"`   //当前页码
+	PageSize  int             `p:"pageSize"`  //每页数
+	Ctx       context.Context `swaggerignore:"true"`
+	OrderBy   string          //排序方式
 }
 
 type UpFile struct {
