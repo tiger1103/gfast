76dd4fd2378c66cde6cae312d222f2d28023fb61
yxh <yxh1103@qq.com>
2021-12-22 11:43:02 +0800

    插件路由拆分

---
+++ plugins/router/router.go
@@ -0,0 +1,8 @@
+/*
+* @desc:插件路由
+* @company:云南省奇讯科技有限公司
+* @Author: yixiaohu
+* @Date:   2021/12/22 10:42
+ */
+
+package router
---
+++ router/plugins.go
@@ -0,0 +1,11 @@
+/*
+* @desc:插件路由
+* @company:云南省奇讯科技有限公司
+* @Author: yixiaohu
+* @Date:   2021/8/26 10:28
+ */
+
+package router
+
+//插件路由加载
+import _ "gfast/plugins/router"
