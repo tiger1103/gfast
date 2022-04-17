53fc4003144193727fdf6497dff7e01de52124c7
yxh <yxh1103@qq.com>
2022-04-17 22:49:10 +0800

    字典类型

--- api/v1/system/sys_dict_type.go
+++ api/v1/system/sys_dict_type.go
@@ -15,7 +15,7 @@ import (
 )
 
 type DictTypeSearchReq struct {
-	g.Meta   `path:"/system/dict/type/list" tags:"字典管理" method:"get" summary:"字典类型列表"`
+	g.Meta   `path:"/dict/type/list" tags:"字典管理" method:"get" summary:"字典类型列表"`
 	DictName string `p:"dictName"` //字典名称
 	DictType string `p:"dictType"` //字典类型
 	Status   string `p:"status"`   //字典状态
@@ -29,7 +29,7 @@ type DictTypeSearchRes struct {
 }
 
 type DictTypeAddReq struct {
-	g.Meta   `path:"/system/dict/type/add" tags:"字典管理" method:"post" summary:"添加字典类型"`
+	g.Meta   `path:"/dict/type/add" tags:"字典管理" method:"post" summary:"添加字典类型"`
 	DictName string `p:"dictName"  v:"required#字典名称不能为空"`
 	DictType string `p:"dictType"  v:"required#字典类型不能为空"`
 	Status   uint   `p:"status"  v:"required|in:0,1#状态不能为空|状态只能为0或1"`
@@ -40,7 +40,7 @@ type DictTypeAddRes struct {
 }
 
 type DictTypeGetReq struct {
-	g.Meta `path:"/system/dict/type/get" tags:"字典管理" method:"get" summary:"获取字典类型"`
+	g.Meta `path:"/dict/type/get" tags:"字典管理" method:"get" summary:"获取字典类型"`
 	DictId uint `p:"dictId" v:"required#类型id不能为空"`
 }
 
@@ -50,7 +50,7 @@ type DictTypeGetRes struct {
 }
 
 type DictTypeEditReq struct {
-	g.Meta   `path:"/system/dict/type/edit" tags:"字典管理" method:"put" summary:"修改字典类型"`
+	g.Meta   `path:"/dict/type/edit" tags:"字典管理" method:"put" summary:"修改字典类型"`
 	DictId   int64  `p:"dictId" v:"required|min:1#主键ID不能为空|主键ID必须为大于0的值"`
 	DictName string `p:"dictName"  v:"required#字典名称不能为空"`
 	DictType string `p:"dictType"  v:"required#字典类型不能为空"`
@@ -62,7 +62,7 @@ type DictTypeEditRes struct {
 }
 
 type DictTypeDeleteReq struct {
-	g.Meta  `path:"/system/dict/type/delete" tags:"字典管理" method:"delete" summary:"删除字典类型"`
+	g.Meta  `path:"/dict/type/delete" tags:"字典管理" method:"delete" summary:"删除字典类型"`
 	DictIds []int `p:"dictIds" v:"required#字典类型id不能为空"`
 }
 
